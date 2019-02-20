daa2eda6e46997e75c5ea6cd2d9378b5750a5195
Nicho Rivera <nicho@knewhealth.com>
2019-02-20 11:12:01 -0700

    adds git ignore rules

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 const express = require('express');
-const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
+const cors = require('cors');
+const dotenv = require('dotenv').config();
 var app = express();
+var PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3000;
+mongoose.Promise = global.Promise;
+
+//mongo connection events
+mongoose.connection.on('connected', () => {
+  console.log('Connection Established')
+})
+
+mongoose.connection.on('reconnected', () => {
+  console.log('Connection Reestablished')
+})
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Connection Disconnected')
+})
+
+mongoose.connection.on('close', () => {
+  console.log('Connection Closed')
+})
+
+mongoose.connection.on('error', (error) => {
+  console.log('ERROR: ' + error)
+})
+
+const run = async () => {
+  await mongoose.connect(process.env.MONGO_URI, {
+    autoReconnect: true,
+    reconnectTries: 1000000,
+    reconnectInterval: 3000
+  })
+}
+
+run().catch(error => console.error(error))
 
 app.use(express.static('dist'))
 
